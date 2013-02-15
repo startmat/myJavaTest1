3179e18f94967856f712ca311194e376abdc4510
elem <dev@utanet.at>
2013-02-15 01:51:55 +0100

    change on master

--- myJava/src/eu/loeb/StartMe.java
+++ myJava/src/eu/loeb/StartMe.java
@@ -7,7 +7,7 @@ public class StartMe {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world 1");
+		System.out.println("Hello world 111");
 		System.out.println("Hello world 2");		
 
 	}
