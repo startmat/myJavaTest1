06ac13077d9acaab2e6dc0d3c6898eef8afe949c
elem <dev@utanet.at>
2013-02-15 01:47:24 +0100

    Sec Branch readme deleted 22 added Sec Branch added

--- myJava/src/eu/loeb/StartMe.java
+++ myJava/src/eu/loeb/StartMe.java
@@ -8,8 +8,9 @@ public class StartMe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world 1");
-		System.out.println("Hello world 2");
+		System.out.println("Hello world 22");
 		System.out.println("First Branch");
+		System.out.println("Sec Branch");
 
 	}
 
