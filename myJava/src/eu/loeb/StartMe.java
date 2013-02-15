060e4388ae48f069c313583900a2376a5527fa96
elem <dev@utanet.at>
2013-02-15 01:43:25 +0100

    Commit on first Branch

--- myJava/src/eu/loeb/StartMe.java
+++ myJava/src/eu/loeb/StartMe.java
@@ -8,7 +8,8 @@ public class StartMe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world 1");
-		System.out.println("Hello world 2");		
+		System.out.println("Hello world 2");
+		System.out.println("First Branch");
 
 	}
 
