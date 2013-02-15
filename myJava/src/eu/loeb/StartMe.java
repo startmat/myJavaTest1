2bf56e536fe2adea7abf0f99d9f48e06b21b7dd4
elem <dev@utanet.at>
2013-02-15 01:33:24 +0100

    This is a commit message

--- myJava/src/eu/loeb/StartMe.java
+++ myJava/src/eu/loeb/StartMe.java
@@ -7,7 +7,8 @@ public class StartMe {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Hello world 1");
+		System.out.println("Hello world 2");		
 
 	}
 
